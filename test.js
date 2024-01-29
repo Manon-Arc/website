87764162b84f472e1275577cfe3e3911de11c740
Manon-Arc <112950709+Manon-Arc@users.noreply.github.com>
2024-01-29 21:24:30 +0100

    add rotate element test ok

--- test.js
+++ test.js
@@ -8,10 +8,10 @@ let rayon = 100;
 function animer() {
     const rect = container.getBoundingClientRect();
     const containerCenterX = rect.left + rect.width / 2 + element.offsetWidth / 2;
-    const containerCenterY = rect.top + rect.height / 2;
+    const containerCenterY = rect.top + rect.height / 2 + element.offsetHeight / 2;
 
     const x = containerCenterX + rayon * Math.cos(angle) - element.offsetWidth / 2;
-    const y = containerCenterY + rayon * Math.sin(angle) - element.offsetHeight / 2; 
+    const y = containerCenterY + rayon * Math.sin(angle) - element.offsetHeight / 2;
 
     element.style.left = x + 'px';
     element.style.top = y + 'px';
@@ -21,12 +21,32 @@ function animer() {
     requestAnimationFrame(animer);
 }
 
-container.addEventListener('mouseenter', function() {
-    rayon = 220;
+container.addEventListener('mouseenter', function () {
+    boucleUp(220)
+
 });
 
-container.addEventListener('mouseleave', function() {
-    rayon = 100;
+container.addEventListener('mouseleave', function () {
+    boucleDown(100)
 });
 
 animer();
+
+function boucleUp(NewRayon) {
+    if (rayon < NewRayon) {
+        rayon++;
+        setTimeout(function() {
+            boucleUp(NewRayon);
+        }, 0.5);
+    }
+}
+
+function boucleDown(NewRayon) {
+    if (rayon > NewRayon) {
+        rayon--;
+        setTimeout(function() {
+            boucleDown(NewRayon);
+        }, 0.5);
+    }
+}
+
