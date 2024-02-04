867f7e36a4eea2cca50e783e8d3a60d8532b1db9
Manon-Arc <112950709+Manon-Arc@users.noreply.github.com>
2024-02-04 21:20:41 +0100

    add anim elec

--- js/test.js
+++ js/test.js
@@ -1,42 +1,32 @@
-const gear = document.getElementById('gear');
-const pcb = document.getElementById('pcb');
-const container = document.getElementById('container');
+const gear = document.getElementById('fusion');
+const container = document.getElementById('meca-contain');
+const containerRect = container.getBoundingClientRect();
 
 const vitesse = 0.01;
 let angleG = 0;
-let angleP = 180;
 let rayon = 100;
 
 function animer() {
-    const rect = container.getBoundingClientRect();
-    const containerCenterX = rect.left + rect.width / 2 + gear.offsetWidth / 2;
-    const containerCenterY = rect.top + rect.height / 2 + gear.offsetHeight / 2;
+    const containerCenterX = containerRect.left + containerRect.width / 2;
+    const containerCenterY = containerRect.top + containerRect.height / 2;
 
     const xGear = containerCenterX + rayon * Math.cos(angleG) - gear.offsetWidth / 2;
     const yGear = containerCenterY + rayon * Math.sin(angleG) - gear.offsetHeight / 2;
 
-    const xPcb = containerCenterX + rayon * Math.cos(angleP) - pcb.offsetWidth / 2;
-    const yPcb = containerCenterY + rayon * Math.sin(angleP) - pcb.offsetHeight / 2;
-
     gear.style.left = xGear + 'px';
     gear.style.top = yGear + 'px';
 
-    pcb.style.left = xPcb + 'px';
-    pcb.style.top = yPcb + 'px';
-
-    angleP += vitesse;
     angleG += vitesse;
 
     requestAnimationFrame(animer);
 }
 
 container.addEventListener('mouseenter', function () {
-    boucleUp(220)
-
+    boucleUp(170);
 });
 
 container.addEventListener('mouseleave', function () {
-    boucleDown(100)
+    boucleDown(100);
 });
 
 animer();
@@ -58,4 +48,3 @@ function boucleDown(NewRayon) {
         }, 0.5);
     }
 }
-
