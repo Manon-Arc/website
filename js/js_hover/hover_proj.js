c2739519b7cb0e4fdfd56ef64ef7cbb683e684d1
Manon-Arc <112950709+Manon-Arc@users.noreply.github.com>
2024-02-22 23:29:37 +0100

    correct import script

--- js/js_hover/hover_proj.js
+++ js/js_hover/hover_proj.js
@@ -1,21 +1,16 @@
 export function addOpacityListeners(target) {
 
-    const projectDesc = document.querySelector('.project-desc.' + target);
-    console.log(projectDesc)
-    const projectDetail = document.querySelector('.project-detail.' + target);
-    console.log(projectDetail)
-
-    const projectLink = document.querySelector('.link-zone.' + target);
+    var projectDesc = document.querySelector('.project-desc.' + target);
+    var projectDetail = document.querySelector('.project-detail.' + target);
+    var projectLink = document.querySelector('.link-zone.' + target);
 
 projectDesc.addEventListener('mouseenter', () => {
     projectDesc.style.opacity = '0';
-    console.log("enyer")
     projectDetail.style.opacity = '1';
 });
 
 projectDesc.addEventListener('mouseleave', () => {
     projectDesc.style.opacity = '1';
-    console.log("exit")
     projectDetail.style.opacity = '0';
 });
 
