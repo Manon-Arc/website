378820c32df83cf483c79436d7cafafdae29ac34
Manon-Arc <arcasmanon47@gmail.com>
2024-02-24 20:31:40 +0100

    test for on addOpacitylistener with for

--- js/load_content.js
+++ js/load_content.js
@@ -18,14 +18,41 @@ function selectedCategory(divCliquee, url) {
     loadContent(url)
         .then(() => {
             console.log(divCliquee.classList);
-            console.log(divCliquee.classList[1]);
-            for (let i = 0; i < divCliquee.classList[1].length; i++) {
-                addOpacityListeners(divCliquee.classList[i])
+            console.log(divCliquee.classList[0]);
+
+            switch (divCliquee.classList[0]) {
+                case "cat1":
+                    for (let i = 0; i < cat1.length; i++) {
+                        addOpacityListeners(cat1[i]);
+                    }
+                    break;
+                case "cat2":
+                    for (let i = 0; i < cat2.length; i++) {
+                        addOpacityListeners(cat2[i]);
+                    }
+                    break;
+                case "cat3":
+                    for (let i = 0; i < cat3.length; i++) {
+                        addOpacityListeners(cat3[i]);
+                    }
+                    break;
+                case "cat4":
+                    for (let i = 0; i < cat4.length; i++) {
+                        addOpacityListeners(cat4[i]);
+                    }
+                    break;
+                default:
+                    console.log("Classe non reconnue :", divCliquee.classList[0]);
+                    break;
             }
-            count++;
+
+            count++; // Incrémentation du compteur après le traitement
+
         })
         .catch(error => console.error('Error:', error));
 }
+
+
 
 function loadContent(url) {
     return fetch(url)
